75bdafe7fdcb83e1e76aa2a2379dda65504dd0bb
Dhanraj.P <dhanrajp3531@gmail.com>
2017-05-15 12:49:02 +0530

    final

---
+++ chap4/4.9.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void ungetch(int);
+int getch(void);
+void ungets(void);
+
+int buf;
+int c;
+
+int main()
+{
+ ungets();
+}
+
+void ungets(void)
+{
+ c = getch();
+ ungetch(c);
+ printf("%c\n", getch());
+}
+
+
+void ungetch(int x)
+{
+ if (buf != 0)
+     printf("cannot ungetch\n");
+ else
+     buf = x;
+}
+
+int getch(void)
+{
+ int p;
+ p = (buf != 0) ? buf:getchar();
+ return p;
+}
